cd73a9f68bf953ac15345d58a363f7b527dab943
Marko Viitanen <fador@iki.fi>
2018-11-08 23:26:45 +0200

    Update example command for Audio streaming

--- example/test.cmd
+++ example/test.cmd
@@ -1,1 +1,11 @@
-ffmpeg -re -i out_test.mkv -an -f rawvideo -pix_fmt yuv420p - | rtpcli.exe
+rem extract raw audio 48000Hz 16bit stereo
+rem ffmpeg -i out_test.mkv -f s16le -acodec pcm_s16le audio.raw
+
+rem Video only
+rem ffmpeg -re -i out_test.mkv -an -f rawvideo -pix_fmt yuv420p - | rtpcli.exe
+
+rem Audio only
+rem rtpcli.exe - 8888 8899 audio.raw
+
+rem Video and Audio
+ffmpeg -re -i out_test.mkv -an -f rawvideo -pix_fmt yuv420p - | rtpcli.exe - 8888 8899 audio.raw
